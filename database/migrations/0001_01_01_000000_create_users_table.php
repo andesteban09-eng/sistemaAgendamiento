fd2c6712647a014b1c4f4e23c4250f193c223b3a
Andersson Esteban Peña <andi.esteban09@gmail.com>
2026-08-20 14:30:20 -0500

    cambios en las migraciones de la tabla users

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -11,18 +11,18 @@ return new class extends Migration
      */
     public function up(): void
     {
-       Schema::create('users', function (Blueprint $table) {
-    $table->id();
-    $table->string('name');
-    $table->string('email')->unique();
-    $table->timestamp('email_verified_at')->nullable();
-    $table->string('password');
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
 
-    $table->string('rol')->default('Paciente');
-    $table->string('estado')->default('Activo');
+            $table->string('rol')->default('Paciente');
+            $table->string('estado')->default('Activo');
 
-    $table->rememberToken();
-    $table->timestamps();
+            $table->rememberToken();
+            $table->timestamps();
         });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
@@ -31,13 +31,21 @@ return new class extends Migration
             $table->timestamp('created_at')->nullable();
         });
 
-        Schema::create('sessions', function (Blueprint $table) {
-            $table->string('id')->primary();
-            $table->foreignId('user_id')->nullable()->index();
-            $table->string('ip_address', 45)->nullable();
-            $table->text('user_agent')->nullable();
-            $table->longText('payload');
-            $table->integer('last_activity')->index();
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('name');
+            $table->string('apellido');
+
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+
+            $table->string('rol')->default('Paciente');
+            $table->string('estado')->default('Activo');
+
+            $table->rememberToken();
+            $table->timestamps();
         });
     }
 
