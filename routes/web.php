7a62016ab4c1012a22f2325cbd3a4fa1f325b132
Ivandavd <ivandavidf2018@gmail.com>
2026-08-21 15:02:27 -0500

    Implementar dashboard paciente, actualizar configuracion de base de datos y ajustar los modelos

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,19 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\dashboardController;
+use App\Livewire\Actions\Logout;
+use Illuminate\Http\Request;
 
 Route::get('/', function () {
     return view('inicio');
 })->name('inicio');
 
-Route::view('dashboard', 'dashboard')
+Route::get('dashboard', [dashboardController::class, 'index'])
     ->middleware(['auth', 'verified'])
     ->name('dashboard');
 
-    Route::middleware(['auth', 'role:Administrador'])->group(function () {
+Route::middleware(['auth', 'role:Administrador'])->group(function () {
 
     Route::get('/admin', function () {
         return view('admin.dashboard');
@@ -22,10 +25,18 @@ Route::view('dashboard', 'dashboard')
 
 });
 
+Route::post('logout', function (Request $request, Logout $logout) {
+    $logout();
+
+    return redirect('/');
+})->middleware('auth')->name('logout');
+
 Route::view('profile', 'profile')
     ->middleware(['auth'])
     ->name('profile');
 
 require __DIR__.'/auth.php';
+
+
 
 
